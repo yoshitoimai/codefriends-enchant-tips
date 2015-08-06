3196d8a226e138f8788ce3f790ebf85ad58f25f6
Yoshito Imai <yoshito.imai@gmail.com>
2015-08-06 16:58:23 +0900

    add player and enemy

--- custom.js
+++ custom.js
@@ -1,7 +1,25 @@
 preroadImage = function(core) {
 
+	core.preload('asteroid_1.png', 'player.png');
+
 };
 
 gameStart = function(core) {
+
+	var asteroid = new ActionSprite('asteroid_1.png', 32, 32);
+
+	asteroid.x = 144;
+	asteroid.y = 0;
+
+	asteroid.tl.moveBy(0, 300, 50);
+
+	core.rootScene.addChild(asteroid);
+
+	var player = new ActionSprite('player.png', 32, 32);
+
+	player.x = 144;
+	player.y = 300;
+
+	core.rootScene.addChild(player);
 
 };
